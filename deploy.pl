08fdafbdcc556bd2f4c29ffe72b223d5998441ee
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-12 07:03:09 +0900

    Perlクラブのロゴを追加

--- deploy.pl
+++ deploy.pl
@@ -4,7 +4,7 @@ use utf8;
 
 my @args = @ARGV;
 
-my $deploy_cmd = q(ssh prod_perl_club_sites 'git -C ~/www/en_webdesign-public fetch && git -C ~/www/en_webdesign-public reset --hard origin/master');
+my $deploy_cmd = q(ssh prod_perl_club_sites 'git -C ~/www/webdesign-public fetch && git -C ~/www/webdesign-public reset --hard origin/master');
 
 system($deploy_cmd) == 0
   or die "Can't execute deploy command: $deploy_cmd:$!";
